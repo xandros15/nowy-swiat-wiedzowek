f63f31e97d6d54374209a5a60d4ae54cc11088e7
xandros15 <xandros92@gmail.com>
2018-12-26 20:08:47 +0100

    refactor frontend, add some styles, move assets

---
+++ public/javascripts/script.js
@@ -0,0 +1,60 @@
+$(function () {
+    const socket = io();
+    const player = {nickname: '', isAdmin: false};
+    const $users = $('#users');
+    const $answer = $('#answer');
+    const $login = $('#login');
+    const $reset = $('#reset');
+
+    socket.on('game.admin-login', () => {
+        $login.hide();
+        $answer.hide();
+        $users.show();
+        $reset.show();
+        player.isAdmin = true;
+    });
+
+    socket.on('game.player', p => {
+        $login.hide();
+        $answer.show();
+        $users.hide();
+        player.nickname = p.nickname;
+    });
+
+    socket.on('game.login-failed', () => {
+        $('#password').val('');
+        alert('Player exist or wrong password');
+    });
+
+    socket.on('game.reset', () => {
+        $answer.find('input').val('');
+    });
+
+    socket.on('game.players-update', players => {
+        $users.html('');
+        for (const player of players) {
+            const $player = $(`<li>Player: <b>${player.nickname}</b> Answer: <b>${player.answer}</b></li>`);
+            $users.append($player);
+        }
+    });
+
+    $login.submit(e => {
+        e.preventDefault();
+        socket.emit('game.login', {
+            nickname: $login.find('#nickname').val(),
+            password: $login.find('#password').val(),
+        });
+    });
+
+    $answer.submit(e => {
+        e.preventDefault();
+        socket.emit('game.answer', {
+            message: $answer.find('input').val(),
+        });
+    });
+
+    $reset.click(e => {
+        e.preventDefault();
+        socket.emit('game.reset');
+    });
+});

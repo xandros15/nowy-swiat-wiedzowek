a4fefa6646d13059808ca86a1e678a6f1380a327
xandros15 <xandros92@gmail.com>
2019-11-26 13:59:19 +0100

    change list of answers to table

--- admin/src/components/AnswersPage.vue
+++ admin/src/components/AnswersPage.vue
@@ -1,12 +1,26 @@
 <template>
     <div>
         <h2>Odpowiedzi</h2>
-        <button @click="reset">Resetuj</button>
-        <ul>
-            <li :key="k" v-for="(answer, k) in answers">
-                {{answer.nickname}} a: {{answer.answer}} m: {{answer.answerAlt}}
-            </li>
-        </ul>
+        <div v-if="answers.length > 0">
+            <button @click="reset">Resetuj</button>
+            <table>
+                <thead>
+                <tr>
+                    <th>Drużyna</th>
+                    <th>Anime</th>
+                    <th>Dodatkowo</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr :key="k" v-for="(answer, k) in answers">
+                    <td>{{answer.nickname}}</td>
+                    <td>{{answer.answer}}</td>
+                    <td>{{answer.answerAlt}}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <h3 v-else>Nie ma jeszcze odpowiedzi.</h3>
     </div>
 </template>
 

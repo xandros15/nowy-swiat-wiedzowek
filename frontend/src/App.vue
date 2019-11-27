f30d0580ccc8175643359e22b52c6cc9db8d204d
xandros15 <xandros92@gmail.com>
2019-11-27 12:22:57 +0100

    add basic and reset styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -23,4 +23,18 @@
 </script>
 
 <style lang="scss">
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+
+    #app, html, body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+    }
 </style>

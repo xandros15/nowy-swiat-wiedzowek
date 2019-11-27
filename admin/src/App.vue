d001300b8215916a3099184908f34cdad4911bef
xandros15 <xandros92@gmail.com>
2019-11-27 13:05:47 +0100

    add style to login to admin panel

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -23,4 +23,28 @@
 </script>
 
 <style lang="scss">
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+
+    #app, html, body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+    }
+
+    body {
+        background: #b85f1b; /* fallback for old browsers */
+        background: -webkit-linear-gradient(right, #b85f1b, #c2874d);
+        background: -moz-linear-gradient(right, #b85f1b, #c2874d);
+        background: -o-linear-gradient(right, #b85f1b, #c2874d);
+        background: linear-gradient(to left, #b85f1b, #c2874d);
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
 </style>
--- admin/src/components/LoginPage.vue
+++ admin/src/components/LoginPage.vue
@@ -1,8 +1,11 @@
 <template>
-    <form @submit.prevent="loginAdmin">
-        <input placeholder="password" title="password" type="password" v-model="password">
-        <button>Login</button>
-    </form>
+    <div class="login-container">
+        <form @submit.prevent="loginAdmin" class="login-form">
+            <h2 class="title">Admin panel</h2>
+            <input class="login-form-input" placeholder="hasło" title="hasło" type="password" v-model="password"/>
+            <button class="login-form-button">Zaloguj się</button>
+        </form>
+    </div>
 </template>
 
 <script>
@@ -19,6 +22,51 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    .login {
+        &-container {
+            width: 360px;
+            padding: 8% 0 0;
+            margin: auto;
+        }
 
+        &-form {
+            position: relative;
+            z-index: 1;
+            background: #fff;
+            max-width: 360px;
+            margin: 0 auto 100px;
+            padding: 45px;
+            text-align: center;
+            box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+
+            &-input {
+                outline: 0;
+                background: #f2f2f2;
+                width: 100%;
+                border: 0;
+                margin: 0 0 15px;
+                padding: 15px;
+                box-sizing: border-box;
+                font-size: 14px;
+            }
+
+            &-button {
+                text-transform: uppercase;
+                outline: 0;
+                background: #b84d08;
+                width: 100%;
+                border: 0;
+                padding: 15px;
+                color: #fff;
+                font-size: 14px;
+                cursor: pointer;
+
+                &:hover, &:active, &:focus {
+                    background: #d15208;
+
+                }
+            }
+        }
+    }
 </style>

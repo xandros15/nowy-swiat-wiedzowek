5bdaf18b923b84030c875eeaa30cdcda497407ce
xandros15 <xandros92@gmail.com>
2019-11-27 12:55:16 +0100

    add style to answer page

--- frontend/src/components/AnswerPage.vue
+++ frontend/src/components/AnswerPage.vue
@@ -1,15 +1,20 @@
 <template>
-    <form @submit.prevent="sendAnswer">
-        <div>
-            <label for="answer">Anime: </label>
-            <input :value="answer" id="answer" ref="answer">
+    <div class="answer-wrapper">
+        <div class="answer-container">
+            <form @submit.prevent="sendAnswer" class="answer-form">
+                <div>
+                    <label class="answer-form-label" for="answer">Anime: </label>
+                    <input :value="answer" autocomplete="off" class="answer-form-input" id="answer" ref="answer">
+                </div>
+                <div>
+                    <label class="answer-form-label" for="answer-alt">Dodatkowe dane: </label>
+                    <input :value="answerAlt" autocomplete="off" class="answer-form-input" id="answer-alt"
+                           ref="answerAlt">
+                </div>
+                <button class="answer-form-button">Wyślij</button>
+            </form>
         </div>
-        <div>
-            <label for="answer-alt">Dodatkowe dane: </label>
-            <input :value="answerAlt" id="answer-alt" ref="answerAlt">
-        </div>
-        <button>Wyślij</button>
-    </form>
+    </div>
 </template>
 
 <script>
@@ -37,5 +42,55 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    .answer {
+        &-container {
+            width: 360px;
+            padding: 8% 0 0;
+            margin: auto;
+        }
+
+        &-form {
+            position: relative;
+            z-index: 1;
+            background: #fff;
+            max-width: 360px;
+            margin: 0 auto 100px;
+            padding: 45px;
+            text-align: center;
+            box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+
+            &-label {
+                font-weight: bold;
+            }
+
+            &-input {
+                outline: 0;
+                background: #f2f2f2;
+                width: 100%;
+                border: 0;
+                margin: 0 0 15px;
+                padding: 15px;
+                box-sizing: border-box;
+                font-size: 14px;
+            }
+
+            &-button {
+                text-transform: uppercase;
+                outline: 0;
+                background: #b84d08;
+                width: 100%;
+                border: 0;
+                padding: 15px;
+                color: #fff;
+                font-size: 14px;
+                cursor: pointer;
+
+                &:hover, &:active, &:focus {
+                    background: #d15208;
+
+                }
+            }
+        }
+    }
 </style>

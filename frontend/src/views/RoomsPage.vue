2d0c4cf2614a8dd3a0a1aa83b26211722c2a3986
xandros15 <xandros92@gmail.com>
2019-11-28 15:24:32 +0100

    add dynamic room list

--- frontend/src/views/RoomsPage.vue
+++ frontend/src/views/RoomsPage.vue
@@ -1,9 +1,14 @@
 <template>
     <div class="room-list">
-        <h1 :key="room" v-for="room in rooms">
-            <router-link :to="{name: 'TeamPage', params: {room}}" class="room-link">
-                {{room}}
-            </router-link>
+        <div v-if="rooms.length > 0">
+            <h1 :key="room" v-for="room in rooms">
+                <router-link :to="{name: 'TeamPage', params: {room}}" class="room-link">
+                    {{room}}
+                </router-link>
+            </h1>
+        </div>
+        <h1 class="h1" v-else>
+            Przepraszam, nie ma aktualnie otwartych pokoi.
         </h1>
     </div>
 </template>
@@ -11,15 +16,28 @@
 <script>
   export default {
     name: "RoomsPage",
+    created () {
+      fetch('/rooms.json')
+        .then(r => r.json())
+        .then(rooms => this.rooms = rooms)
+        .catch(() => this.rooms = [])
+    },
     data () {
       return {
-        rooms: ['remcon', 'xmascon']
+        rooms: []
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
+    .h1 {
+        font-size: 2rem;
+        color: #f2f2f2;
+        text-shadow: 2px 2px 2px #000;
+        text-decoration: none;
+    }
+
     .room-link {
         font-size: 120%;
         color: #f2f2f2;

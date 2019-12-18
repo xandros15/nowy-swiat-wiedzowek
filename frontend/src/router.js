8193e3af1ef4efddc3298f9cad1e7bd28992a92b
xandros15 <xandros92@gmail.com>
2019-12-18 18:13:18 +0100

    add score page for other page

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import AdminPage from './views/AdminPage'
 import RoomsPage from './views/RoomsPage'
+import ScorePage from './views/ScorePage'
 import TeamPage from './views/TeamPage'
 
 Vue.use(VueRouter)
@@ -19,6 +20,12 @@ const router = new VueRouter({
       path: '/:room(\\w{3,8})/admin',
       name: 'AdminPage',
       component: AdminPage,
+      props: true,
+    },
+    {
+      path: '/:room(\\w{3,8})/score',
+      name: 'ScorePage',
+      component: ScorePage,
       props: true,
     },
     {
---
+++ frontend/src/views/ScorePage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+        <div v-if="isLogged && isAdmin">
+            <h2 class="title">Punktacja</h2>
+            <div class="flex">
+                <Score/>
+            </div>
+        </div>
+        <LoginAdminPage :room="room" v-else/>
+    </div>
+</template>
+
+<script>
+  import { mapState } from 'vuex'
+  import Score from '../components/Score'
+  import LoginAdminPage from '../components/LoginAdminPage'
+
+  export default {
+    name: 'ScorePage',
+    components: {Score, LoginAdminPage},
+    props: ['room'],
+    computed: mapState(['isLogged', 'isAdmin']),
+  }
+</script>
+
+<style lang="scss" scoped>
+    .title {
+        color: #f2f2f2;
+        text-shadow: 2px 2px 2px #020202;
+    }
+
+    .flex {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
